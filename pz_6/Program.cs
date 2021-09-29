555a5e83fb97d5bdec81c3a391d301051c363bff
Loli Kross <lizalove3535@gmail.com>
2021-09-29 13:31:56 +0500

    test edit

---
+++ pz_6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace pz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7;
+            int b = 1;
+            while (b < 10) 
+            {
+                Console.WriteLine($"{a}*{b}=" +a*b);
+                b++;
+            }
+        }
+    }
+}
