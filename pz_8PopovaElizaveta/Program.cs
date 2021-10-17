e88150eef5d6b9327f9d1dfb49e8f53ac70e3b1b
Loli Kross <lizalove3535@gmail.com>
2021-10-17 13:11:17 +0500

    aoao

---
+++ pz_8PopovaElizaveta/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace pz_8PopovaElizaveta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ConsoleKeyInfo keyInfo;
+            int x;
+            int[] A = new int[20];
+            Random random = new Random();
+            A[0] = random.Next(30);
+            for (x = 0; x < A.Length; x++)
+            {
+                while (x <= 19)
+                {
+                    A[x] = random.Next(30);
+                    Console.WriteLine(A[x]);
+                    x++;
+                }
+
+            }
+            for (int i = 1; i < 20;)
+            {
+                int n = random.Next(30);
+                for (x = 0; x < i; x++)
+                {
+                    if (n == A[x])
+                        break; 
+                }
+                if (x == i)
+                { 
+                    A[i] = n;
+                    i++;
+                }
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write(A[i] + " ");
+            }
+        }
+    }
+}
---
+++ pz_9PopovaElizaveta/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace pz_9PopovaElizaveta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            int[,] A = new int [n, n];
+            int i, j;
+            Random random = new Random();
+            for (i = 0; i <=(n-1); i++)
+            {
+                for (j = 0; j <=(n-1); j++)
+                {
+                    A[i, j] = random.Next(1,10);
+                    Console.Write(A[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            int f = 1;
+            int h = 0;
+            bool r;
+            for (i = 1; i <=(n-1); i++)
+            {
+                for (j = 1; j <=(n-1); j++)
+                {
+                    do
+                    {
+                        r = f.Equals(A[i, j]);
+                        if (r == true)
+                        {
+                            Console.WriteLine(f);
+                            break;
+                        }
+                        else
+                        {
+                            h++;
+                        }
+                    }  
+                    while (i == n - j + h);
+
+                }
+            }
+            
+            //for (int s = 1; s <=10; s++)
+            //{
+
+            
+            //    for (i = 1; i <= (n - 1); i++)
+
+            //    {
+                
+            //        for (j = 1; j <= (n - 1); j++)
+            //        {
+            //            int k = 0;
+            //            if ((i <= n - j + k) & (s == A[i, j]))
+            //            {
+            //                Console.Write(s);
+                        
+            //                break;
+                        
+            //            } else
+            //            {
+            //                k++;
+
+            //            }
+            //        }
+            //    }
+            //}
+        }
+    }
+}
