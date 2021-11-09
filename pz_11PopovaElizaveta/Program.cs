3c18c13121ef948a220876a52660330461d920ac
Loli Kross <lizalove3535@gmail.com>
2021-11-09 10:00:02 +0500

    test edit

---
+++ pz_11PopovaElizaveta/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace pz_11PopovaElizaveta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int i, j;
+            char[] D = new char[a.Length];
+            for  (j = 0; j < D.Length; j++)
+            {
+                for (i = 0; i < a.Length; i++) 
+                {
+                    D[j] = a[i];
+                    if (Char.IsNumber(D[j]) == true)
+                    {
+                        Console.WriteLine(D[j]);
+                    }
+                }
+                break;
+            }
+            
+        }
+    }
+}
--- pz_8PopovaElizaveta/Program.cs
+++ pz_8PopovaElizaveta/Program.cs
@@ -8,16 +8,55 @@ namespace pz_8PopovaElizaveta
         static void Main(string[] args)
         {
             
-            int i;
-            int[] A = new int[20];
+            int i, j;
+            int a = j = 0;
+            int[] F = new int[10];
+            int[] A = new int[10];
             Random random = new Random();
             for (i = 0; i < A.Length; i++)
             {
-                A[i] = random.Next(30);
-                Console.Write($"{A[i]} ");
+                    A[i] = random.Next(-20, 20);
+                    Console.Write($"{A[i]} ");
+                    if (A[i]<0)
+                    {
+                        a++;
+                    }
             }
-            var outArr = A.Distinct();
-            foreach (int x in outArr) Console.Write($"{x}\t");
+            Console.WriteLine();
+            Console.Write(a);
+            Console.WriteLine();
+            for (j = 0; j < F.Length; j++)
+            {
+                for (i = 0; i < A.Length; i++)
+                {
+                    if (A[i]<0)
+                    {
+                        F[j]=A[i];
+                        Console.Write($"{F[j]}");
+                    }
+                }
+                break;
+            }
+            for (j = 0; j < F.Length; j++)
+            {
+                int current = F[j];
+                int k = j-1;
+                while (k >= 0 && current > F[k])
+                {
+                    F[k+1] = F[k];
+                    k--;
+                }
+                F[k+1] = current;
+            }
+            Console.WriteLine();
+            foreach (var VARIABLE in F)
+            {
+                Console.Write(" " + VARIABLE);
+            }
+            
+            
+            //var outArr = A.Distinct();
+            //foreach (int x in outArr) Console.Write($"{x}\t");
         }
     }
 }
