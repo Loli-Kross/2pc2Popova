1dd250fbd647e43a25f705a2fb68a0c9f3541416
Loli Kross <lizalove3535@gmail.com>
2021-11-25 10:16:47 +0500

    test edit

---
+++ pz_14PopovaElizaveta/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace pz_14PopovaElizaveta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int i, j, d;
+            d = 0;
+            string path = @"C:\SomeDir3\";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            using (FileStream inFile = new FileStream($"{path}inFile.txt", FileMode.Create))
+            {
+                byte[] array = System.Text.Encoding.Default.GetBytes(a);
+                inFile.Write(array, 0, array.Length);
+            }
+            using (FileStream onFile = File.OpenRead($"{path}inFile.txt"))
+            {
+                byte[] array = new byte [onFile.Length];
+                onFile.Read(array, 0, array.Length);
+                string t = System.Text.Encoding.Default.GetString(array);
+                Console.WriteLine($"{t}\n");
+                for(i = 0; i < array.Length; i++)
+                {
+                    for (j = 0; j<array.Length; j++) 
+                    { 
+                        char s = t[i];
+                        if (char.IsNumber(s) == true)
+                        {
+                            d++;
+                        }
+                        break;
+                    }
+                }
+                Console.WriteLine($"{d}");
+            }
+            
+        }
+    }
+}
---
+++ pz_15PopovaElizaveta/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace pz_15PopovaElizaveta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader(@"C:\SomeDir3\inFile.txt"))
+            {
+                string line;
+                string[] str;
+                int sum = 0;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    sum++;
+                }
+                Console.WriteLine("Количество строк: " + sum);
+            }
+            Console.WriteLine();
+            using (StreamReader reader1 = new StreamReader(@"C:\SomeDir3\inFile.txt"))
+            {
+                string line;
+                string[] str;
+                int sum = 0;
+                while ((line = reader1.ReadLine()) != null)
+                {
+                    sum++;
+                    str = line.Split(' ');
+                    Console.WriteLine($"Количество слов в {sum} строке: " + str.Length);
+                    Console.WriteLine($"Количество символов в {sum} строке: " + line.Length);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
