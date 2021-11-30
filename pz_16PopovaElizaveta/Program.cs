6b5f2b9cdd6aeb1ca11ee40f4157d93743657a81
Loli Kross <lizalove3535@gmail.com>
2021-11-30 10:04:43 +0500

    test edit

---
+++ pz_16PopovaElizaveta/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace pz_16PopovaElizaveta
+{
+    class Program
+    {
+        static void Quarter( int x, int y, out int result)
+        {
+            result = 0;
+            if (x > 0)
+            {
+                if (y > 0)
+                    result = 1;
+                else if (y < 0)
+                { 
+                    result = 4;
+                Console.WriteLine(result); }
+            }
+            else
+            {
+                if (y > 0)
+                    result = 2;
+                else if (y < 0) { 
+                    result = 3;
+                Console.WriteLine(result); }
+            }
+            
+        } 
+        static void Main(string[] args)
+        {
+            int r;
+            int x, y;
+            x = 4; y = -10;
+            Quarter(x,  y, out r);
+            x = -8; y = -9;
+            Quarter( x,  y, out r);
+            x = 7; y = 20;
+            Quarter( x,  y, out r);
+        }
+    }
+}
---
+++ pz_17PopovaElizaveta/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace pz_17PopovaElizaveta
+{
+    class Program
+    {
+        static void Main()
+        {
+            string g = "";
+            Mani(g);
+        }
+        static string Mani(string f)
+        {
+            f = @"C:\Windows";
+            Console.WriteLine("Подкаталоги:");
+            string[] S = Directory.GetDirectories(f);
+            foreach (string a in S)
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine("Файлы:");
+            string[] P = Directory.GetFiles(f
+                );
+            foreach (string a in P)
+            {
+                Console.WriteLine(a);
+            }
+            return "";
+        }
+    }
+}
