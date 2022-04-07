473856293947eaeeb0e23f05fbdb03d79e7efe0e
Loli Kross <lizalove3535@gmail.com>
2022-04-07 23:01:05 +0500

    test edit Делигаты

---
+++ 2_8_Delegats/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _2_8_Delegats
+//2.Напишите программу, в которой объявляется делегат для методов с
+//символьным аргументом, не возвращающих результат.Опишите класс, в
+//котором должно быть символьное поле и метод, позволяющий присвоить
+//значение символьному полю объекта. У метода один символьный аргумент и
+//метод не возвращает результат. Создайте массив объектов данного класса.
+//Создайте экземпляр делегата.В список вызовов этого делегата необходимо
+//добавить ссылки на метод (присваивающий значение символьному полю)
+//каждого объекта из массива. Проверить результат вызова такого экземпляра
+//делегата.
+{
+    delegate void Symbol(char c);
+
+    class MyClass
+    {
+        public char myField;
+
+        public void MyMethod(char c)
+        {
+            myField = c;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new MyClass[3] { new MyClass(), new MyClass(), new MyClass() };
+            Symbol del = null;
+            foreach (var item in array)
+            {
+                del += item.MyMethod;
+            }
+            char c = 'o';
+            del(c);
+            foreach (var item in array)
+            {
+                Console.WriteLine(item.myField);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pz_2_8PopovaElizaveta/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+namespace pz_2_8PopovaElizaveta
+{
+    
+
+    //1.Напишите программу, в которой объявляется делегат для методов с
+    //двумя аргументами(символ и текст) и целочисленным результатом.В
+    //главном классе необходимо описать два статических метода. Один
+    //статический метод результатом возвращает количество вхождений символа
+    //(первый аргумент) в текстовую строку(второй аргумент).Другой метод
+    //результатом возвращает индекс первого вхождения символа(первый
+    //аргумент) в текстовую строку(второй аргумент), или значение -1, если
+    //символ в текстовой строке не встречается.В главном методе создать
+    //экземпляр делегата и с помощью этого экземпляра вызвать каждый из
+    //статических методов.
+
+        delegate int Operation(char c, string s);
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                char c = 'o';
+                string s = "hello world";
+                Operation del = Entrance;
+                int result = del(c, s);
+                Console.WriteLine(result);
+
+                del = Index;
+                result = del(c, s);
+                Console.WriteLine(result);
+                Console.ReadKey();
+            }
+
+            // 1
+            private static int Entrance(char c, string s)
+            {
+                int num = 0;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (c == s[i])
+                        num++;
+                }
+                if (num == 0)
+                    return -1;
+                return num;
+            }
+            // 2
+            private static int Index(char c, string s)
+            {
+                return s.IndexOf(c);
+            }
+        }
+}
